3c04c32c837fb397a8cc654a64ddf41de8e287d5
tyronechrisharris <142608718+tyronechrisharris@users.noreply.github.com>
2026-04-27 19:51:41 -0400

    Add reset-node.sh for Docker cleanup and NODE_NAME increment
    This script stops all running Docker containers, cleans the Docker system, removes specified directories and files, and increments the NODE_NAME in the .env file.

---
+++ dist/release/reset-node.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Stop all running containers
+docker stop $(docker ps -q)
+
+# Deep clean docker system
+docker system prune --all --volumes -f
+
+# Remove directories and log file
+sudo rm -rf hivemq-config/ osh-node-oscar/ pgdata/ dist/ logs.txt
+
+# Increment the NODE_NAME number in .env
+sed -i -E 's/(NODE_NAME=.*)([0-9]+)$/echo "\1$((\2+1))"/e' .env
+
+echo "Cleanup complete and NODE_NAME incremented."
